193bc2903c4ba1d1bc76e7da7a5d84697577ec2f
Greg Tatham <tathamg@gmail.com>
2025-12-25 11:48:22 -0500

    Updates (forgot exactly what)

--- src/coolledx/message_handler.py
+++ src/coolledx/message_handler.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 
 import abc
 
-
 class MessageHandler(abc.ABC):
     """Abstract class for message handler."""
 
@@ -18,4 +17,39 @@ class CoolLEDXMessageHandler(MessageHandler):
 
     def handle_message(self, message: bytearray) -> None:
         """Handle a message from the device."""
-        print(f"Received message: {message}")
+        print(f"Received message: {message}")   
+
+
+class CoolLEDMMessageHandler(MessageHandler):
+    """Message handler for CoolLEDM devices."""
+
+    def handle_message(self, message: bytearray) -> None:
+        """Handle a message from the device."""
+        print(f"Received message: {message}")   
+
+
+class CoolLEDUMessageHandler(MessageHandler):
+    """Message handler for CoolLEDU devices."""
+
+    def handle_message(self, message: bytearray) -> None:
+        """Handle a message from the device."""
+        print(f"Received message: {message}")   
+
+
+
+class CoolLEDMXMessageHandler(MessageHandler):
+    """Message handler for CoolLEDMX devices."""
+
+    def handle_message(self, message: bytearray) -> None:
+        """Handle a message from the device."""
+        print(f"Received message: {message}")   
+
+
+class CoolLEDUXMessageHandler(MessageHandler):
+    """Message handler for CoolLEDUX devices."""
+
+    def handle_message(self, message: bytearray) -> None:
+        """Handle a message from the device."""
+        print(f"Received message: {message}")   
+
+
